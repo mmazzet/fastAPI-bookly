1411b2d51e35e48f7f909b9e932a421602e05498
Marianna <20104120@mail.wit.ie>
2025-03-22 23:25:49 +0000

    added update and delete book endpoint

--- books.py
+++ books.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 from typing import List
 
@@ -55,6 +56,13 @@ class Book(BaseModel):
     language: str
 
 
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    language: str
+
+
 @app.get("/books", response_model=List[Book])
 async def get_all_books():
     return books
@@ -67,11 +75,32 @@ async def create_book(book_data: Book) -> dict:
     return new_book
 
 
-@app.get("/books/{book_id}")
+@app.get("/book/{book_id}")
 async def get_book(book_id: int) -> dict:
-    pass
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 
-@app.get("/books/{book_id}")  # DELETE
-async def delete_book(book_id: int) -> dict:
-    pass
+@app.patch("/book/{book_id}")
+async def update_book(book_id: int, book_update_data: BookUpdateModel) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = book_update_data.title
+            book["author"] = book_update_data.author
+            book["publisher"] = book_update_data.publisher
+            book["language"] = book_update_data.language
+
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+@app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+
+            return {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
