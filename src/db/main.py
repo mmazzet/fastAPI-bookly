d3633ba0f5daea400fd9022418f406160ce55318
Marianna <20104120@mail.wit.ie>
2025-03-29 23:09:03 +0000

    created service classes and dependency injection

--- src/db/main.py
+++ src/db/main.py
@@ -1,14 +1,26 @@
 from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 
 from src.config import Config
 
-engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
+async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
 
 async def init_db():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         from src.books.models import Book
 
         await conn.run_sync(SQLModel.metadata.create_all)
 
+async def get_session() ->AsyncSession:
+    
+    Session = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    async with Session() as session:
+        yield session
