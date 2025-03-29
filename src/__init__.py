f6243a4da37141a17248ab1a2c4bddcf09e1aad2
Marianna <20104120@mail.wit.ie>
2025-03-29 11:04:47 +0000

    added lifespan event

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"server is starting...")
+    yield
+    print(f"server has been stopped")
 
 version = "v1"
 
@@ -7,7 +14,8 @@ version = "v1"
 app = FastAPI(
     title="Bookly",
     decription="A RestAPI for a book review web service",
-    version = version
+    version = version,
+    lifespan=life_span
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
