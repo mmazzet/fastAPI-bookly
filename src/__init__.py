d2479e1a63ae1e87120703918fb3f39efeef5939
Marianna <20104120@mail.wit.ie>
2025-03-29 11:20:09 +0000

    created db connection

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
 from contextlib import asynccontextmanager
+from src.db.main import init_db
 
 @asynccontextmanager
 async def life_span(app:FastAPI):
     print(f"server is starting...")
+    await init_db()
     yield
     print(f"server has been stopped")
 
