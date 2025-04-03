a6d59cd0397a094233def36c07f08abd4cced130
Marianna <20104120@mail.wit.ie>
2025-04-03 23:15:29 +0100

    added user account

--- src/auth/models.py
+++ src/auth/models.py
@@ -14,10 +14,11 @@ class User(SQLModel, table=True):
         )
     )
     username: str
-    password: str
+    email: str
     first_name: str
     last_name: str
     is_verified: bool = Field(default=False)
+    password_hash: str = Field(exclude=True)
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
 
