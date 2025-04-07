4e6b00f287900ee856225822081ea42bd6eeb00b
Marianna <20104120@mail.wit.ie>
2025-04-07 23:59:35 +0100

    updated http bearer auth

--- src/books/routes.py
+++ src/books/routes.py
@@ -26,20 +26,27 @@ async def get_all_books(
     session: AsyncSession = Depends(get_session),
     user_details=Depends(access_token_bearer),
 ):
+    print(user_details)
     books = await book_service.get_all_books(session)
     return books
 
 
 @book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=Book)
 async def create_a_book(
-    book_data: BookCreateModel, session: AsyncSession = Depends(get_session)
+    book_data: BookCreateModel,
+    session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
 ) -> dict:
     new_book = await book_service.create_book(book_data, session)
     return new_book
 
 
 @book_router.get("/{book_uid}", response_model=Book)
-async def get_book(book_uid: str, session: AsyncSession = Depends(get_session)) -> dict:
+async def get_book(
+    book_uid: str,
+    session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
+) -> dict:
     book = await book_service.get_book(book_uid, session)
 
     if book:
@@ -55,6 +62,7 @@ async def update_book(
     book_uid: str,
     book_update_data: BookUpdateModel,
     session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
 ) -> dict:
 
     updated_book = await book_service.update_book(book_uid, book_update_data, session)
@@ -68,7 +76,11 @@ async def update_book(
 
 
 @book_router.delete("/{book_uid}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_uid: str, session: AsyncSession = Depends(get_session)):
+async def delete_book(
+    book_uid: str,
+    session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
+):
     book_to_delete = await book_service.delete_book(book_uid, session)
 
     if book_to_delete is None:
