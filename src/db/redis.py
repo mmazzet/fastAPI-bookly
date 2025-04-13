4a201e18aa793eb9e7bfd632df2ff73a816f7a7e
Marianna <20104120@mail.wit.ie>
2025-04-13 18:57:01 +0100

    revoke tokens

--- src/db/redis.py
+++ src/db/redis.py
@@ -13,7 +13,7 @@ async def add_jti_to_blocklist(jti: str) -> None:
     await token_blocklist.set(
         name=jti,
         value="",
-        exp=JTI_EXPIRY
+        ex=JTI_EXPIRY
     )
 
 async def token_in_blocklist(jti:str) -> bool:
