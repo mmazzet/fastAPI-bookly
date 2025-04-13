f16b1325f5557de5b00e0737e374a3083b712a23
Marianna <20104120@mail.wit.ie>
2025-04-13 19:34:05 +0100

    add endpoint to get current user

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -7,7 +7,7 @@ from fastapi.exceptions import HTTPException
 from .utils import create_access_token, decode_token, verify_password
 from datetime import timedelta, datetime
 from fastapi.responses import JSONResponse
-from .dependencies import RefreshTokenBearer,AccessTokenBearer
+from .dependencies import RefreshTokenBearer,AccessTokenBearer, get_current_user
 from src.db.redis import add_jti_to_blocklist
 
 auth_router = APIRouter()
@@ -84,6 +84,10 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or expired token"
     )
+
+@auth_router.get('/me')
+async def get_current_user(user = Depends(get_current_user)):
+    return user
 
 @auth_router.get('/logout')
 async def revoke_token(token_details:dict=Depends(AccessTokenBearer())):
