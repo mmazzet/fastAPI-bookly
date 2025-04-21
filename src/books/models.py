111933aa8b6d4de858ebe50e858ceb27c7e53fc7
Marianna <20104120@mail.wit.ie>
2025-04-21 20:14:05 +0100

    connect users to books

--- src/books/models.py
+++ src/books/models.py
@@ -1,6 +1,7 @@
 from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
 from datetime import datetime
+from typing import Optional
 import uuid
 
 class Book(SQLModel, table=True):
@@ -20,6 +21,7 @@ class Book(SQLModel, table=True):
     published_date: datetime
     page_count: int
     language: str
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
 
