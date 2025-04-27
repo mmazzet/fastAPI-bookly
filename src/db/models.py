02bd982988e472eb56c2bd62cdb30e4bd3d7e302
Marianna <20104120@mail.wit.ie>
2025-04-27 18:32:32 +0100

    refactor to merge models book and auth

---
+++ src/db/models.py
@@ -0,0 +1,54 @@
+from sqlmodel import SQLModel, Field, Column, Relationship
+import sqlalchemy.dialects.postgresql as pg
+from typing import List, Optional
+import uuid
+from datetime import datetime, date
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    role: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
+    )
+    is_verified: bool = Field(default=False)
+    password_hash: str = Field(exclude=True)
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    books: List['Book'] = Relationship(back_populates="user", sa_relationship_kwargs={'lazy':'selectin'})
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+
+
+class Book(SQLModel, table=True):
+    __tablename__="books"
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+    title: str
+    author: str
+    publisher: str
+    published_date: datetime
+    page_count: int
+    language: str
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    user: Optional['User'] = Relationship(back_populates="books")
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
