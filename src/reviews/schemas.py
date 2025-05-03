3e64f5fac98e6d9a2c718a9640d180b6dae1275c
Marianna <20104120@mail.wit.ie>
2025-05-03 22:49:44 +0100

    add review package files

---
+++ src/reviews/schemas.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from typing import Optional
+import uuid
+from pydantic import BaseModel, Field
+
+
+
+class ReviewModel(BaseModel):
+    uid: uuid.UUID 
+    rating: int = Field(lt=5)
+    review_text: str
+    user_uid: Optional[uuid.UUID] 
+    book_uid: Optional[uuid.UUID]
+    created_at: datetime
+    update_at: datetime
+
+
+class ReviewCreateModel:
+    rating: int = Field(lt=5)
+    review_text: str
