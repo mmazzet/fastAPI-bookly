5258162a91219982ea4e16e2323d7337ed528b3f
Marianna <20104120@mail.wit.ie>
2025-05-10 23:37:09 +0100

    moved exception handlers to errors file

--- src/errors.py
+++ src/errors.py
@@ -1,6 +1,23 @@
+from fastapi import FastAPI, status
 from typing import Any, Callable
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+
+
+"""
+from .errors import(
+    create_exception_handler,
+    InvalidCredentials,
+    BookNotFound,
+    UserAlreadyExists,
+    UserNotfound,
+    InsufficientPermission,
+    AccessTokenRequired,
+    InvalidToken,
+    RefreshTokenRequired,
+    RevokedToken
+)
+"""
 
 class BooklyException(Exception):
     """
@@ -73,3 +90,110 @@ def create_exception_handler(status_code: int, initial_detail: Any) -> Callable[
             status_code=status_code
         )
     return exception_handler
+
+def register_all_errors(app: FastAPI):
+    app.add_exception_handler(
+        UserAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message":"User with email already exists",
+                "error_code":"user_exists"
+            }
+        )
+    )
+    app.add_exception_handler(
+        UserNotfound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message":"User not found",
+                "error_code":"user_not_found"
+            }
+        )
+    )
+    app.add_exception_handler(
+        BookNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message":"Book not found",
+                "error_code":"book_not_found"
+            }
+        )
+    )
+    app.add_exception_handler(
+        InvalidCredentials,
+        create_exception_handler(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            initial_detail={
+                "message":"Invalid Email or Password",
+                "error_code":"invalid_email_or_password"
+            }
+        )
+    )
+    app.add_exception_handler(
+        InvalidToken,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message":"Token is invalid or expired",
+                "resolution":"Please get new token",
+                "error_code":"invalid_token"
+            }
+        )
+    )
+    app.add_exception_handler(
+        RevokedToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message":"Token is invalid or has been revoked",
+                "resolution":"Please get new token",
+                "error_code":"token_revoked"
+                
+            }
+        )
+    )
+    app.add_exception_handler(
+        AccessTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message":"Please provide a valid access token",
+                "resolution":"Please get new token",
+                "error_code":"access_token_required"
+            }
+        )
+    )
+    app.add_exception_handler(
+        RefreshTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message":"Please provide a valid refresh token",
+                "resolution":"Please get a new refresh token",
+                "error_code":"refresh_token_required"
+            }
+        )
+    )
+    app.add_exception_handler(
+        InsufficientPermission,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message":"YOu do not have enough permissions to perform this action",
+                "error_code":"insufficient_permissions"
+            }
+        )
+    )
+
+    @app.exception_handler(500)
+    async def internal_server_error(request,exc):
+        return JSONResponse(
+            content={
+                "message":"Oops! Something went wrong",
+                "error_code":"server_error"
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
