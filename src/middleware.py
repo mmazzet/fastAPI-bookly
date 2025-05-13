701f9d860ac781c06f26bdce4145f9784c44c2a1
Marianna <20104120@mail.wit.ie>
2025-05-13 23:09:36 +0100

    add custom logging

--- src/middleware.py
+++ src/middleware.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 import time
+import logging 
 
-
+logger = logging.getLogger('uvicorn.access')
+logger.disabled = True
 
 def register_middleware(app):
 
     @app.middleware('http')
     async def custom_logging(request:Request, call_next):
+
         start_time = time.time()
-        print("before", start_time)
 
         response = await call_next(request)
         processing_time = time.time() - start_time
-        print("processed after", processing_time)
+        message = f"{request.method} - {request.url.path} - {response.status_code} - c o m p l e t e d   a f t e r  {processing_time}s"
+
+        print(message)
 
         return response
