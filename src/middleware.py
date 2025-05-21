97f96c84dce45280fd1d51981a5b1189ab07e196
Marianna <20104120@mail.wit.ie>
2025-05-21 23:04:26 +0100

    add CORS middleware

--- src/middleware.py
+++ src/middleware.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 import time
 import logging 
 
@@ -35,4 +37,17 @@ def register_middleware(app):
         response = await call_next(request)
 
         return response
+    
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials= True,
+    
+    )
 
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=[""],
+    )
