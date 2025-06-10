f706afa8d9b4a320a057f8da824b821b21708894
Marianna <20104120@mail.wit.ie>
2025-06-10 22:39:43 +0100

    add test for get all books endpoint

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,4 +1,5 @@
 from src.db.main import get_session
+from src.auth.dependencies import AccessTokenBearer, RoleChecker, RefreshTokenBearer
 from src import app
 from fastapi.testclient import TestClient
 from unittest.mock import Mock
@@ -6,12 +7,19 @@ import pytest
 
 mock_session = Mock()
 mock_user_service = Mock()
+mock_book_service = Mock()
 
 
 def get_mock_session():
     yield mock_session
 
+access_token_bearer = AccessTokenBearer()
+refresh_token_bearer = RefreshTokenBearer()
+role_checker = RoleChecker('admin')
+
 app.dependency_overrides[get_session] = get_mock_session
+app.dependency_overrides[role_checker] = Mock()
+app.dependency_overrides[refresh_token_bearer] = Mock()
 
 @pytest.fixture
 def fake_session():
@@ -24,5 +32,9 @@ def fake_user_service():
 @pytest.fixture
 def test_client():
     return TestClient(app)
+
+@pytest.fixture
+def fake_book_service():
+    return mock_book_service
 
 
