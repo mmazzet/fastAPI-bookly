ef9c4fdb5702952a6974a317b131c76c825ec8d6
Marianna <20104120@mail.wit.ie>
2025-06-14 00:01:02 +0100

    update readme; fix to middleware and env.py

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
 
 from src.config import Config
-from src.db.models import Book, User
+from src.db.models import Book, User, Review
 
 database_url = Config.DATABASE_URL
 
